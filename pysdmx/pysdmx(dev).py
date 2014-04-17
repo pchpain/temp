61fcbed6269bd4ebf3b053410a57af43e269a2b5
pierre <pchuong3@users.noreply.github.com>
2014-04-17 16:08:47 +0200

    Update pysdmx(dev).py

--- pysdmx/pysdmx(dev).py
+++ pysdmx/pysdmx(dev).py
@@ -11,6 +11,8 @@ import numpy
 import logging
 import pudb #
 import pdb #
+
+
 def date_parser(date, frequency):
     if frequency == 'A':
         return datetime.datetime.strptime(date, '%Y')
@@ -29,9 +31,9 @@ def query_rest(url):
     if request.status_code != requests.codes.ok:
         raise ValueError("Error getting client({})".format(request.status_code))      
     parser = lxml.etree.XMLParser(
-        ns_clean=True, recover=True, encoding='utf-8')
+            ns_clean=True, recover=True, encoding='utf-8')
     return lxml.etree.fromstring(
-        request.text.encode('utf-8'), parser=parser)
+            request.text.encode('utf-8'), parser=parser)
 
 
 class Codelist(object): # done
@@ -44,101 +46,104 @@ class Codelist(object): # done
         if not self._codes:
             self._codes = {}
             codelists = self.tree.xpath(".//message:CodeLists",
-                                          namespaces=self.tree.nsmap)
+                    namespaces=self.tree.nsmap)
             for codelists_ in codelists:
                 for codelist in codelists_.iterfind(".//structure:CodeList",
-                                                    namespaces=self.tree.nsmap):
+                        namespaces=self.tree.nsmap):
                     name = codelist.xpath('.//structure:Name', namespaces=self.tree.nsmap)
                     name = name[0]
                     name = name.text
                     code = []
                     for code_ in codelist.iterfind(".//structure:Code",
-                                                   namespaces=self.tree.nsmap):
+                            namespaces=self.tree.nsmap):
                         code_key = code_.get('value')
                         code_name = code_.xpath('.//structure:Description',
-                                                namespaces=self.tree.nsmap)
+                                namespaces=self.tree.nsmap)
                         code_name = code_name[0]
                         code.append((code_key,code_name.text))
                     self._codes[name] = code
         return self._codes
 
 
-class DataflowsECB(object):  # ok
+class Dataflows(object):  # ok
     def __init__(self, SDMXML):
         self.tree = SDMXML
         self._all_dataflows = None
 
     @property
-    def all_dataflows(self):  #ok
+    def all_dataflows(self):  
         #pudb.set_trace()
         if not self._all_dataflows:
             self._all_dataflows = {}
             for dataflow in self.tree.iterfind(".//structure:Dataflow",
-                                               namespaces=self.tree.nsmap):
+                    namespaces=self.tree.nsmap):
                 id = dataflow.get('id')
                 agencyID = dataflow.get('agencyID')
                 version = dataflow.get('version')
                 #Keyfamilyref = {}
                 name = dataflow.xpath('.//structure:Name', namespaces=self.tree.nsmap)[0].text
                 for keyfamilyref in dataflow.iterfind(".//structure:KeyFamilyRef",
-                                                   namespaces=self.tree.nsmap):
-                    keyfamilyid = dataflow.xpath('.//structure:KeyFamilyID', namespaces=self.tree.nsmap)[0].text
-                    keyfamilyagenceid = dataflow.xpath('.//structure:KeyFamilyAgencyID', namespaces=self.tree.nsmap)[0].text
+                        namespaces=self.tree.nsmap):
+                    keyfamilyid = keyfamilyref.xpath('.//structure:KeyFamilyID', namespaces=self.tree.nsmap)[0].text
+                    keyfamilyagenceid =  keyfamilyref.xpath('.//structure:KeyFamilyAgencyID', namespaces=self.tree.nsmap)[0].text
+                categoryscheme = dataflow.xpath('.//structure:CategorySchemeID', namespaces=self.tree.nsmap)[0].text
+                categoryID = dataflow.xpath('.//structure:ID', namespaces=self.tree.nsmap)[0].text
                 self._all_dataflows[id] = (agencyID, version, name,
-                        keyfamilyid, keyfamilyagenceid)#, categoryschemeid ) 
-        return self._all_dataflows
+                        keyfamilyid,
+                        keyfamilyagenceid,categoryscheme,categoryID ) 
+                return self._all_dataflows
 
 
-class DataECB(object):#ok
+class Data(object):#ok
     def __init__(self, SDMXML):
         self.tree = SDMXML
         self._time_series = None
 
     @property
     def time_series(self):
-        #pudb.set_trace()
         if not self._time_series:
             self._time_series = {}
-            
+
             for group  in self.tree.iterfind(".//generic:Group",
-                                             namespaces=self.tree.nsmap):
+                    namespaces=self.tree.nsmap):
                 for series in group.iterfind(".//generic:Series",
-                                                namespaces=self.tree.nsmap):
+                        namespaces=self.tree.nsmap):
                     codes = {}
                     for key in series.iterfind(".//generic:Value",
-                                            namespaces=self.tree.nsmap):
+                            namespaces=self.tree.nsmap):
                         codes[key.get('concept')] = key.get('value')
                     time_series_ = []
                     for observation in series.iterfind(".//generic:Obs",
-                                                    namespaces=self.tree.nsmap):
+                            namespaces=self.tree.nsmap):
                         dimensions = observation.xpath(".//generic:Time",
-                                                    namespaces=self.tree.nsmap)
+                                namespaces=self.tree.nsmap)
                         dimension = dimensions[0].text 
                         dimension = date_parser(dimension, codes['FREQ'])
                         values = observation.xpath(".//generic:ObsValue",
-                                                namespaces=self.tree.nsmap)
+                                namespaces=self.tree.nsmap)
                         value = values[0].values()
                         value = value[0]
                         observation_status = 'A'
                         for attribute in \
-                            observation.iterfind(".//generic:Attributes",
-                                                namespaces=self.tree.nsmap):
-                            for observation_status_ in \
-                                attribute.xpath(
-                                    ".//generic:Value[@concept='OBS_STATUS']",
-                                    namespaces=self.tree.nsmap):
-                                if observation_status_ is not None:
-                                    observation_status \
-                                        = observation_status_.get('value')
+                                observation.iterfind(".//generic:Attributes",
+                                        namespaces=self.tree.nsmap):
+                                    for observation_status_ in \
+                                            attribute.xpath(
+                                                    ".//generic:Value[@concept='OBS_STATUS']",
+                                                    namespaces=self.tree.nsmap):
+                                                if observation_status_ is not None:
+                                                    observation_status \
+                                                            = observation_status_.get('value')
                         time_series_.append((dimension, value, observation_status))
                     time_series_.sort()
                     dates = numpy.array(
-                        [observation[0] for observation in time_series_])
+                            [observation[0] for observation in time_series_])
                     values = numpy.array(
-                        [observation[1] for observation in time_series_])
+                            [observation[1] for observation in time_series_])
                     time_series_ = pandas.Series(values, index=dates)
                     self._time_series[str(uuid.uuid1())] = (codes, time_series_)
         return self._time_series
+
 
 class Concept(object):  # ok
     def __init__(self, SDMXML):
@@ -151,7 +156,7 @@ class Concept(object):  # ok
         if not self._concept:
             self._concept = {}
             for concept in self.tree.iterfind(".//structure:Concept",
-                                               namespaces=self.tree.nsmap):
+                    namespaces=self.tree.nsmap):
                 id = concept.get('id')
                 agencyID = concept.get('agencyID')
                 version = concept.get('version')
@@ -170,29 +175,30 @@ class Keyfamily(object): # done
         if not self._codes:
             self._codes = {}
             codelists = self.tree.xpath(".//message:CodeLists",
-                                          namespaces=self.tree.nsmap)
+                    namespaces=self.tree.nsmap)
             for codelists_ in codelists:
                 for codelist in codelists_.iterfind(".//structure:CodeList",
-                                                    namespaces=self.tree.nsmap):
+                        namespaces=self.tree.nsmap):
                     name = codelist.xpath('.//structure:Name', namespaces=self.tree.nsmap)
                     name = name[0]
                     name = name.text
                     code = []
                     for code_ in codelist.iterfind(".//structure:Code",
-                                                   namespaces=self.tree.nsmap):
+                            namespaces=self.tree.nsmap):
                         code_key = code_.get('value')
                         code_name = code_.xpath('.//structure:Description',
-                                                namespaces=self.tree.nsmap)
+                                namespaces=self.tree.nsmap)
                         code_name = code_name[0]
                         code.append((code_key,code_name.text))
                     self._codes[name] = code
         return self._codes
 
+
 class Categoryscheme(object): # done
     def __init__(self, SDMXML):
         self.tree = SDMXML
         self._category = None
-    
+
     #def walktree
 
     @property
@@ -200,42 +206,73 @@ class Categoryscheme(object): # done
         if not self._category:
             self._category = {}
             codelists = self.tree.xpath(".//message:CategorySchemes",
-                                          namespaces=self.tree.nsmap)
+                    namespaces=self.tree.nsmap)
             for codelists_ in codelists:
                 for codelist in codelists_.iterfind(".//structure:CategoryScheme",
-                                                    namespaces=self.tree.nsmap):
+                        namespaces=self.tree.nsmap):
                     name = codelist.xpath('.//structure:Name', namespaces=self.tree.nsmap)
                     name = name[0]
                     name = name.text
                     code = []
                     for code_ in codelist.iterfind(".//structure:Category",
-                                                   namespaces=self.tree.nsmap):
+                            namespaces=self.tree.nsmap):
                         code_key = code_.get('id')
                         code_name = code_.xpath('.//structure:Name',
-                                                namespaces=self.tree.nsmap)
+                                namespaces=self.tree.nsmap)
                         code_name = code_name[0]
                         dataflowref=[]
-                        
-                        pdb.set_trace()
-                        for dataflow in code_.iterchildren('DataflowRef'):
-                        #for dataflow in code_.iterind(".//structure:DataflowRef", namespaces=self.tree.nsmap):'''
-                             if dataflow is not None:
+                        #pdb.set_trace()
+                        #for dataflow in
+                        #code_.iterchildren('DataflowRef'):#todo
+                        for dataflow in code_.iterfind(".//structure:DataflowRef", namespaces=self.tree.nsmap):
+                            if dataflow is not None:
                                  dataflowID = dataflow.xpath('.//structure:DataflowID', namespaces=self.tree.nsmap)[0].text
                                  agencyID = dataflow.xpath('.//structure:AgencyID', namespaces=self.tree.nsmap)[0].text
                                  version = dataflow.xpath('.//structure:Version', namespaces=self.tree.nsmap)[0].text
                                  dataflowref.append((agencyID, version,
-                                        dataflowID))
-                        code.append((code_key,code_name.text,dataflowref))
+                                     dataflowID))
+                                 code.append((code_key,code_name.text,dataflowref))
                     self._category[name] = code
         return self._category
 
 
-class SDMX_RESTECB(object): #ok
+class Organisationschemes(object): #TO TEST
+    def __init__(self, SDMXML):
+        self.tree = SDMXML
+        self._organisationscheme = None
+
+    @property
+    def codes(self):
+        if not self._organisationscheme:
+            self._organisationscheme = {}
+            codelists = self.tree.xpath(".//message:OrganisationSchemes",
+                    namespaces=self.tree.nsmap)
+            for codelists_ in codelists:
+                for codelist in codelists_.iterfind(".//structure:CodeList",
+                        namespaces=self.tree.nsmap):
+                    name = codelist.xpath('.//structure:Name', namespaces=self.tree.nsmap)
+                    name = name[0]
+                    name = name.text
+                    code = []
+                    for code_ in codelist.iterfind(".//structure:Agency",
+                            namespaces=self.tree.nsmap):
+                        code_key = code_.get('id')
+                        code_name = code_.xpath('.//structure:Name',
+                                namespaces=self.tree.nsmap)
+                        code_name = code_name[0]
+                        code.append((code_key,code_name.text))
+                    self._organisationscheme[name] = code
+        return self._organisationscheme
+
+
+class SDMX_REST(object): 
 
     def __init__(self, sdmx_url, agencyID):
         self.sdmx_url = sdmx_url
         self.agencyID = agencyID
         self._dataflow = None
+        self._organisationscheme = None  
+    
     def dataflow(self,resourceID = None): #ok
         if not self._dataflow:
             if resourceID is not None :
@@ -243,38 +280,43 @@ class SDMX_RESTECB(object): #ok
                 url = (self.sdmx_url+'/' 
                    + resource + '/'
                    + resourceID)             
-                self._dataflow = DataflowsECB(query_rest(url))
+                self._dataflow = Dataflows(query_rest(url))
             else :
                 resource = 'Dataflow'  
                 url = (self.sdmx_url + '/'
                    + resource  )
-                self._dataflow = DataflowsECB(query_rest(url))    
+                self._dataflow = Dataflows(query_rest(url))    
         return self._dataflow
 
+    @property
+    def data_organisationscheme(self): #in progress
+        if not self._organisationscheme:
+            resource = 'OrganisationScheme'  
+            url = (self.sdmx_url + '/'
+            + resource  )
+            self._organsiationscheme = Organisationschemes(query_rest(url))    
+        return self._organisationscheme
 
     def data_extraction(self, flowRef, freq, key,  startperiod=None,
             endperiod=None):#ok
         resourcetype = 'GenericData'
         resource ='dataflow'
-        #pudb.set_trace()
-        #if not self._dataflow:
         if freq is not None and startperiod is not None and endperiod is not None :
-        	query = (self.sdmx_url + '/'
-                    + resourcetype + '?'
-                    + resource + '='
-                    + flowRef + '&'
-                    + 'FREQ=' +  freq 
-                    + '&CURRENCY='+ key
-                    + '&startTime=' + startperiod
-                    + '&endTime=' + endperiod)
+            query = (self.sdmx_url + '/'
+                + resourcetype + '?'
+                + resource + '='
+                + flowRef + '&'
+                + 'FREQ=' +  freq 
+                + '&CURRENCY='+ key
+                + '&startTime=' + startperiod
+                + '&endTime=' + endperiod)
         else:
-        	query = (self.sdmx_url + '/'
-                    + resourcetype + '?'
-                    + resource + '='
-                    + flowRef)
+            query = (self.sdmx_url + '/'
+                + resourcetype + '?'
+                + resource + '='
+                + flowRef)
         url = (query)
-        print('url :',url)
-        return DataECB(query_rest(url))
+        return Data(query_rest(url))
 
     def data_concept(self, flowRef=None):#done
         resource = 'Concept'
@@ -318,7 +360,6 @@ class SDMX_RESTECB(object): #ok
                + resource) 
         return Categoryscheme(query_rest(url))
 
-
     def data_definition(self, flowRef):#todo
         resource = 'CodeList'
         url = (self.sdmx_url + '/'
@@ -326,12 +367,6 @@ class SDMX_RESTECB(object): #ok
                + flowRef)
         return DSD(query_rest(url))
 
-'''eurostat = SDMX_REST('http://www.ec.europa.eu/eurostat/SDMX/diss-web/rest',
-                     'ESTAT')
-titi=eurostat.data_extraction('cdh_e_fos','..PC.FOS1.BE','2005','2011')
-titi.time_series'''
-#toto=eurostat.dataflowECB
-#   toto.all_dataflows
-#   titi=pysdmx.query_rest('http://sdw-ws.ecb.europa.eu/Dataflow')
 
-ECB = SDMX_RESTECB('http://sdw-ws.ecb.europa.eu','ECB')
+ECB = SDMX_REST('http://sdw-ws.ecb.europa.eu','ECB')
+
